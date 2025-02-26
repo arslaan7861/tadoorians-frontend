722fbee7ac97e8661a48e274f83bd17b9ed350a6
ARSLAAN ANSARI <105743264+arslaan7861@users.noreply.github.com>
2025-02-26 18:22:15 +0530

    fixed eslint errors

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,4 @@
-"use client";
-
-import { Utensils } from "lucide-react";
-import Link from "next/link";
-import { useSelector } from "react-redux";
-import { RootState } from "@/State";
-
 export default function RestaurantTables() {
-  const { tables } = useSelector((state: RootState) => state.tables);
-
   return (
     <div className="bg-background text-textColor flex flex-col items-center justify-center w-full overflow">
       Admin homepage
