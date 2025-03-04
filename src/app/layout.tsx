8734fb0652d2d6ff93be1e6a2299ac81941718c4
ARSLAAN ANSARI <105743264+arslaan7861@users.noreply.github.com>
2025-03-04 10:30:51 +0530

    added server ananlytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Titlebar from "@/components/RootComponents/Titlebar";
 import { ThemeProvider } from "@/components/theme/provider";
 import StateProvider from "@/State";
+import { Analytics } from "@vercel/analytics/react";
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -16,7 +17,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       <ThemeProvider>
         <div className="flex flex-col h-svh">
           {/* <ToastProvider /> */}
-
+          <Analytics />
           <Titlebar />
           {children}
         </div>
