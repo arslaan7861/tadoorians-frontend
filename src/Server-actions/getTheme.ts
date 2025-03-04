88bef7ad4ceeb0e120a8d9bab3f35b053b57090a
ARSLAAN ANSARI <105743264+arslaan7861@users.noreply.github.com>
2025-03-04 13:41:01 +0530

    tried applying own solution

--- src/Server-actions/getTheme.ts
+++ src/Server-actions/getTheme.ts
@@ -1,17 +1,32 @@
 "use server";
-
-import { themeType } from "@/utils/types";
 import { cookies } from "next/headers";
+import { themeType } from "@/utils/types";
 
-const THEME_KEY = "theme";
+export async function getThemeCookie() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  return (theme?.value as themeType) || null;
+}
+export async function setThemeCookie(theme: string) {
+  console.log("saving theme");
+  const cookieStore = await cookies();
+  cookieStore.set("theme", theme);
+  console.log(theme);
+}
+// "use server";
 
-export const setThemeCookie = async (theme: string) => {
-  (await cookies()).set(THEME_KEY, theme, {
-    path: "/",
-    maxAge: 365 * 24 * 60 * 60,
-  });
-};
+// import { themeType } from "@/utils/types";
+// import { cookies } from "next/headers";
 
-export const getThemeCookie = async (): Promise<themeType | null> => {
-  return ((await cookies()).get(THEME_KEY)?.value as themeType) || null;
-};
+// const THEME_KEY = "theme";
+
+// export const setThemeCookie = async (theme: string) => {
+//   (await cookies()).set(THEME_KEY, theme, {
+//     path: "/",
+//     maxAge: 365 * 24 * 60 * 60,
+//   });
+// };
+
+// export const getThemeCookie = async (): Promise<themeType | null> => {
+//   return ((await cookies()).get(THEME_KEY)?.value as themeType) || null;
+// };
