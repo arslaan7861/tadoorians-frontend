d225cb9d152dca4049713fbb7748e5f415dcf29c
ARSLAAN ANSARI <105743264+arslaan7861@users.noreply.github.com>
2025-03-11 19:47:50 +0530

    basic notification

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Tandoorians",
+    short_name: "Tandoorians",
+    description: "A family restaurant",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import PWA from "@/components/RootComponents/PWA";
 import React from "react";
 
 function MenuPage() {
-  return <div className="text-textColor">MenuPage</div>;
+  return (
+    <div className="text-textColor">
+      <PWA />
+    </div>
+  );
 }
 
 export default MenuPage;
